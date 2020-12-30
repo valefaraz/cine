19319b0d95a784b25b30e9eeab45ffedb32a8ffb
valefaraz <valefaraz@gmail.com>
2020-12-29 21:33:33 -0300

    configuracion del admin

--- proyectoCine/cine/admin.py
+++ proyectoCine/cine/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from cine.models import *
 
 # Register your models here.
+
+#admin.site.register(Pelicula)
+#admin.site.register(Sala)
+#admin.site.register(Proyeccion)
+#admin.site.register(Butacas)
+#admin.site.register(ButacasAdmin)
+
+
+
+class PeliculaAdmin(admin.ModelAdmin):
+    list_display = ('pk','name','start_date', 'end_date', 'status')
+    list_filter = ('status','start_date')
+
+
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('pk','name', 'row', 'seat')
+    list_filter = ('status',)
+
+
+
+class ProyeccionAdmin(admin.ModelAdmin):
+    list_display = ('pk','sala','pelicula','start_date', 'end_date', 'status')
+    list_filter = ('status', 'pelicula')
+
+
+
+class ButacasAdmin(admin.ModelAdmin):
+    list_display = ('pk','proyeccion','date', 'row', 'seat')
+    list_filter = ('date', 'proyeccion')
+
+
+admin.site.register(Pelicula, PeliculaAdmin)
+admin.site.register(Sala, SalaAdmin)
+admin.site.register(Proyeccion, ProyeccionAdmin)
+admin.site.register(Butacas, ButacasAdmin)
--- proyectoCine/cine/models.py
+++ proyectoCine/cine/models.py
@@ -12,12 +12,28 @@ class Pelicula(models.Model):
     start_date = models.DateField()  # fecha desde cuando se encontrará disponible la película
     end_date = models.DateField()  # fecha hasta cuando se encuentra disponible la película
 
+    class Meta:
+        verbose_name = "Pelicula"
+        verbose_name_plural = "Peliculas"
+        ordering = ["pk"]
+
+    def __str__(self):
+        return self.name
+
 
 class Sala(models.Model):
     name = models.CharField(max_length=70, blank=False)  # nombre de la sala
     status = models.CharField(max_length=15)  # habilitada, deshabilitada, eliminada
     row = models.IntegerField()  # cantidad de filas que tiene la sala
     seat = models.IntegerField()  # cantidad de asientos que hay en una fila
+
+    class Meta:
+        verbose_name = "Sala"
+        verbose_name_plural = "Salas"
+        ordering = ["pk"]
+
+    def __str__(self):
+        return self.name
 
 
 class Proyeccion(models.Model):
@@ -28,9 +44,24 @@ class Proyeccion(models.Model):
     sala = models.ForeignKey(Sala, on_delete=models.CASCADE)
     pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "Proyeccion"
+        verbose_name_plural = "Proyecciones"
+        ordering = ["pk"]
+
+
+    def __str__(self):
+        a = str(self.pk) + "->" + str(self.pelicula.name)
+        return a
+
 
 class Butacas(models.Model):
     proyeccion = models.ForeignKey(Proyeccion, on_delete=models.CASCADE)
     date = models.DateField()  # fecha de la venta/reserva de la entrada
     row = models.IntegerField()  # fila dentro de la sala
     seat = models.IntegerField()  # asiento dentro de la fila
+
+    class Meta:
+        verbose_name = "Butaca"
+        verbose_name_plural = "Butacas"
+        ordering = ["pk"]
